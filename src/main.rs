68e1720b31f3f7c7090c5a3e26004855ac717c1d
Will Nilges <wdn5796@rit.edu>
2023-11-17 21:23:31 -0500

    Set up GPIO HD44780

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use esp_idf_hal::delay::{FreeRtos, BLOCK};
-use esp_idf_hal::i2c::*;
+use esp_idf_hal::delay::Ets;
+use esp_idf_hal::gpio::*;
 use esp_idf_hal::peripherals::Peripherals;
-use esp_idf_hal::prelude::*;
+
+use hd44780_driver::{HD44780, DisplayMode, Cursor, CursorBlink, Display};
+
 
 fn main() -> anyhow::Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -13,31 +15,36 @@ fn main() -> anyhow::Result<()> {
 
     log::info!("Hello, world!");
 
-    const LCD_ADDRESS: u8 = 0x27; // Address depends on hardware, see link below
-
-    // Create a I2C instance, needs to implement embedded_hal::blocking::i2c::Write, this
-    // particular uses the arduino_hal crate for avr microcontrollers like the arduinos.
-    /*
-    let dp = arduino_hal::Peripherals::take().unwrap();
-    let pins = arduino_hal::pins!(dp);
-    let mut i2c = arduino_hal::I2c::new(
-        dp.TWI, //
-        pins.a4.into_pull_up_input(), // use respective pins
-        pins.a5.into_pull_up_input(),
-        50000,
-    );
-    let mut delay = arduino_hal::Delay::new();
-    */
+    //const LCD_ADDRESS: u8 = 0x27; // Address depends on hardware, see link below
 
     let peripherals = Peripherals::take()?;
-    let i2c = peripherals.i2c0;
-    let sda = peripherals.pins.gpio5;
-    let scl = peripherals.pins.gpio6;
+    //let i2c = peripherals.i2c0;
+    //let sda = peripherals.pins.gpio5;
+    //let scl = peripherals.pins.gpio6;
 
-    println!("Starting I2C test");
+    println!("Holy shit it's willard. I'm doing GPIO things.");
 
-    let config = I2cConfig::new().baudrate(100.kHz().into());
-    let mut i2c = I2cDriver::new(i2c, sda, scl, &config)?;
+    //let config = I2cConfig::new().baudrate(100.kHz().into());
+    //let mut i2c = I2cDriver::new(i2c, sda, scl, &config)?;
+
+    let mut lcd_register = PinDriver::output(peripherals.pins.gpio13)?;
+    let mut lcd_enable = PinDriver::output(peripherals.pins.gpio12)?;
+    
+    let mut lcd_d4 = PinDriver::output(peripherals.pins.gpio14)?;
+    let mut lcd_d5 = PinDriver::output(peripherals.pins.gpio27)?;
+    let mut lcd_d6 = PinDriver::output(peripherals.pins.gpio26)?;
+    let mut lcd_d7 = PinDriver::output(peripherals.pins.gpio25)?;
+
+    let mut lcd = HD44780::new_4bit(
+        lcd_register,
+        lcd_enable,
+        lcd_d4,
+        lcd_d5,
+        lcd_d6,
+        lcd_d7,
+        &mut Ets,
+    );
+    
 
     Ok(())
 }
